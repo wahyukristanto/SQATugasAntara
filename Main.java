66cc938e54bd140aac24e3794c5a3c958b43a850
andreerlando <36688462+andreerlando@users.noreply.github.com>
2018-05-14 09:01:56 +0700

    Update Main.java
    update code main

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+package main;
+
+import Dadu;
+
+
 public class Main {
   public static void main(String[] args) {
-  
+    Dadu cobadadu = new Dadu();
+    
+	for(int i=0; i<7; i++){
+      
+cobadadu.lempar();
+    }
+ 
+System.out.println(cobadadu.toString());
   }
 }
